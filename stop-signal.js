d207742e1e5cf8d46db92a6dcddee17f13098bff
ShacharHochman <58937060+ShacharHochman@users.noreply.github.com>
2020-04-26 10:49:12 +0300

    Update stop-signal.js

--- stop-signal.js
+++ stop-signal.js
@@ -17,9 +17,9 @@ define(['pipAPI'], function(APIconstructor) {
     // Transform logs into a string
     // we save as CSV because qualtrics limits to 20K characters and this is more efficient.
     serialize: function (name, logs) {
-        var headers = ['AA', 'BB', 'CC', 'DD', 'EE', 'FF', 'GG', 'HH', 'II', 'JJ', 'KK', 'LL'];
-        var content = logs.map(function (log) { return [log.name, log.responseHandle, log.latency, log.stimuli, log.media, log.data.score, log.data.response, log.data.type, log.data.type,log.data.correct, log.data.exp, log.data.practice]; });
-    content.unshift(headers);
+        var headers = ['group', 'latency', 'block', 'stimulus', 'correct'];
+        var content = logs.map(function (log) { return [log.name, log.latency, log.stimuli, log.data.type, log.data.score]; });
+        content.unshift(headers);
         return toCsv(content);
 
         function toCsv(matrice) { return matrice.map(buildRow).join('\n'); }
