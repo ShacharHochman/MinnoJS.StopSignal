cf5b44c5c146bdabca96daadc0fd5045ea5ba20c
ShacharHochman <58937060+ShacharHochman@users.noreply.github.com>
2020-04-28 11:46:21 +0300

    Update stop-signal.js

--- stop-signal.js
+++ stop-signal.js
@@ -5,40 +5,40 @@ define(['pipAPI'], function(APIconstructor) {
     var current = API.getCurrent();
     API.addSettings('onEnd', window.minnoJS.onEnd);
     API.addSettings('logger', {
-    // gather logs in array
-    onRow: function(logName, log, settings, ctx){
-        if (!ctx.logs) ctx.logs = [];
-        ctx.logs.push(log);
-    },
-    // onEnd trigger save (by returning a value)
-    onEnd: function(name, settings, ctx){
-        return ctx.logs;
-    },
-    // Transform logs into a string
-    // we save as CSV because qualtrics limits to 20K characters and this is more efficient.
-    serialize: function (name, logs) {
-        var headers = ['group', 'latency', 'block', 'stimulus', 'correct'];
-        var content = logs.map(function (log) { return [log.name, log.latency, log.media, log.data.response, log.data.score]; });
-        content.unshift(headers);
-        return toCsv(content);
+        // gather logs in array
+        onRow: function(logName, log, settings, ctx){
+            if (!ctx.logs) ctx.logs = [];
+            ctx.logs.push(log);
+        },
+        // onEnd trigger save (by returning a value)
+        onEnd: function(name, settings, ctx){
+            return ctx.logs;
+        },
+        // Transform logs into a string
+        // we save as CSV because qualtrics limits to 20K characters and this is more efficient.
+        serialize: function (name, logs) {
+            var headers = ['SlideName', 'SSD', 'Response', 'RT', 'StimData', 'MediaData', 'Correct', 'data.response', 'Stop.NoStop', 'CorrectResponse', 'KK', 'LL','StartExp'];
+            var content = logs.map(function (log) { return [log.name, log.data.stop_signal_time, log.responseHandle, log.latency, log.stimuli, log.media, log.data.score, log.data.response, log.data.type,log.data.correct, log.data.exp]; });
+            content.unshift(headers);
+            return toCsv(content);
 
-        function toCsv(matrice) { return matrice.map(buildRow).join('\n'); }
-        function buildRow(arr) { return arr.map(normalize).join(','); }
-        // wrap in double quotes and escape inner double quotes
-        function normalize(val) {
-            var quotableRgx = /(\n|,|")/;
-            if (quotableRgx.test(val)) return '"' + val.replace(/"/g, '""') + '"';
-            return val;
+            function toCsv(matrice) { return matrice.map(buildRow).join('\n'); }
+            function buildRow(arr) { return arr.map(normalize).join(','); }
+            // wrap in double quotes and escape inner double quotes
+            function normalize(val) {
+                var quotableRgx = /(\n|,|")/;
+                if (quotableRgx.test(val)) return '"' + val.replace(/"/g, '""') + '"';
+                return val;
+            }
+        },
+        // Set logs into an input (i.e. put them wherever you want)
+        send: function(name, serialized){
+            window.minnoJS.logger(serialized);
         }
-    },
-    // Set logs into an input (i.e. put them wherever you want)
-    send: function(name, serialized){
-        window.minnoJS.logger(serialized);
-    }
-});
+    });
 
 
-    var version_id      = Math.random()>0.5 ? 2 : 1;
+    var version_id      = 1;
 
     var all_answers     = [['d', 'l', 'x'],['l', 'd', 'x']];
     var answers     = all_answers[version_id-1];
